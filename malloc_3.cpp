994053ca5983de9e19a15d7fa6749189b92a9911
OmarAbuHgool <117207843+OmarAbuHgool@users.noreply.github.com>
2024-03-27 10:42:19 +0200

    split prototype

--- malloc_3.cpp
+++ malloc_3.cpp
@@ -51,7 +51,8 @@ public:
 
     MeList(); // constructor
     void append(MallocMetadata *element);
-    void add_node_free(MallocMetadata *new_node);
+    void add_node_by_adress(MallocMetadata *new_node);
+    void add_node_all(MallocMetadata *new_node);
     void delete_node(MeList *list, MallocMetadata *node);
     void remove_head();
 };
@@ -84,7 +85,7 @@ void MeList ::append(MallocMetadata *element)
     length++;
 }
 
-void MeList::add_node_free(MallocMetadata *new_node)
+void MeList::add_node_by_adress(MallocMetadata *new_node)
 {
     MallocMetadata *tmp = dummy_head;
     for (int i = 0; i < length; i++)
@@ -100,17 +101,17 @@ void MeList::add_node_free(MallocMetadata *new_node)
 
 void MeList::remove_head()
 {
-    if (dummy_head == nullptr)
+    if (dummy_head == NULL)
     {
         return;
     }
     else
     {
         MallocMetadata *tmp = dummy_head;
-        if (tmp->next != nullptr)
+        if (tmp->next != NULL)
         {
             dummy_head = tmp->next;
-            dummy_head->prev = nullptr;
+            dummy_head->prev = NULL;
         }
     }
 }
@@ -121,12 +122,12 @@ MeList *free_blocks_arr[MAX_ORDER + 1]; // should we initialize it?
 MeList *all_blocks_list;                // each elemnt is a tree "reveresed tree"
 MeList *mmap_list;
 
-void init_blocks()
+void init_blocks(void *source_address)
 {
     for (int i = 0; i < INITIAL_BLOCKS_NUM; i++)
     {
-        MallocMetadata *element;
-        element->size = MAX_SIZE;
+        MallocMetadata *element = (MallocMetadata *)((char *)source_address + (i * MAX_SIZE));
+        element->size = MAX_SIZE - sizeof(MallocMetadata);
         element->is_free = true;
         element->next = NULL;
         element->prev = NULL;
@@ -134,16 +135,21 @@ void init_blocks()
         element->prev_free = NULL;
         element->address = i * MAX_SIZE;
         all_blocks_list->append(element);
+        free_blocks_arr[MAX_ORDER]->append(element);
     }
 }
 
-MallocMetadata *find_block(int order)
+MallocMetadata *find_prefect_node(int order)
 {
     MallocMetadata *helper = all_blocks_list->dummy_head;
     while (order != 0)
         helper = helper->next;
     return helper;
 }
+
+//--------------------------------------------------------------------------------------------------------------------
+//                                          merge part
+//--------------------------------------------------------------------------------------------------------------------
 
 void add_and_merge_buddies(MallocMetadata *element, int order) // recursion approved
 {
@@ -156,11 +162,11 @@ void add_and_merge_buddies(MallocMetadata *element, int order) // recursion appr
     {
         // we should merge
         MallocMetadata *father = merge_all(element);
-        if (father != nullptr)
+        if (father != NULL)
         {
             // by father here .. i just want to get adress of the left buddy
             merge_free(father, order);
-            free_blocks_arr[order + 1]->add_node_free(father);
+            free_blocks_arr[order + 1]->add_node_by_adress(father);
             add_and_merge_buddies(father, order + 1);
         }
     }
@@ -185,7 +191,7 @@ void merge_free(MallocMetadata *node_in_free, int order) // approved
                 next->prev_free = prev;
             if (prev)
                 prev->next_free = next;
-            if((next == NULL) && (prev== NULL))
+            if ((next == NULL) && (prev == NULL))
                 cur_list->dummy_head = NULL;
             break;
         }
@@ -199,7 +205,7 @@ MallocMetadata *merge_all(MallocMetadata *node_in_all) // approved
 {
     MallocMetadata *next_node = node_in_all->next;
     MallocMetadata *prev_node = node_in_all->prev;
-    int xor_result = node_in_all->address ^ (int)node_in_all->size;
+    int xor_result = node_in_all->address ^ ((int)(node_in_all->size + sizeof(MallocMetadata)));
     if (next_node != NULL)
     {
         // note about xor is at the beginning
@@ -245,12 +251,12 @@ MallocMetadata *merge_all(MallocMetadata *node_in_all) // approved
     return NULL;
 }
 
-void add_to_arr(MallocMetadata *element) // approved
+void check_merge(MallocMetadata *element) // approved
 {
     int order = 0;
     while (order < MAX_ORDER)
     {
-        if (pow(2, order) == element->size) // assumig we can use this, asked in PIAZZA already
+        if ((pow(2, order) * 128) == (int)(element->size + sizeof(MallocMetadata))) // assumig we can use this, asked in PIAZZA already
         {
             // we are done
             break;
@@ -261,42 +267,128 @@ void add_to_arr(MallocMetadata *element) // approved
     add_and_merge_buddies(element, order);
 }
 
-void split_buddies(MallocMetadata *element, int order, int size)
+//--------------------------------------------------------------------------------------------------------------------
+//                                          split part
+//--------------------------------------------------------------------------------------------------------------------
+
+void split_all(MallocMetadata *element, MallocMetadata *buddy_of_element)// 5
 {
-    if (order == 0)
+    MallocMetadata *cur_node = all_blocks_list->dummy_head;
+    for (int i = 0; i < all_blocks_list->length; i++)
     {
-        return;
+        if (cur_node == element)
+        {
+            MallocMetadata *next = cur_node->next_free;
+            if (next)
+                next->prev = buddy_of_element;
+            buddy_of_element->next = next;
+            buddy_of_element->prev = cur_node;
+            cur_node->next = buddy_of_element;
+            break;
+        }
+        cur_node = cur_node->next;
     }
-    if ((pow(2, order - 1) < size) && (size <= pow(2, order)))
+    all_blocks_list->length++;
+}
+
+// returns buddy_of_element so i can add it to all_blocks_list
+MallocMetadata *split_free(MallocMetadata *element, int order)// 4
+{
+    MeList *cur_list = free_blocks_arr[order];
+    MallocMetadata *cur_node = cur_list->dummy_head;
+    for (int i = 0; i < cur_list->length; i++)
     {
-        // now we have order which fits the size,
-        element->is_free = false;
-        return;
+        if (cur_node == element)
+        {
+            // removing from order
+            MallocMetadata *next = cur_node->next_free;
+            MallocMetadata *prev = cur_node->prev_free;
+            if (next)
+                next->prev_free = prev;
+            if (prev)
+                prev->next_free = next;
+            if ((next == NULL) && (prev == NULL))
+                cur_list->dummy_head = NULL;
+            cur_list->length--;
+
+            // adding to order - 1
+            element->size = (element->size - sizeof(MallocMetadata)) / 2;
+            //(char*)all_blocks_list->dummy_head) this is the actual physical address
+            MallocMetadata *buddy_of_element = (MallocMetadata *)((char *)all_blocks_list->dummy_head + element->address + element->size + sizeof(MallocMetadata));
+            buddy_of_element->address = (int)(element->address + element->size + sizeof(MallocMetadata));
+            buddy_of_element->size = element->size;
+            buddy_of_element->is_free = true;
+            free_blocks_arr[order - 1]->add_node_by_adress(element);
+            free_blocks_arr[order - 1]->add_node_by_adress(buddy_of_element);
+            return buddy_of_element;
+        }
+        cur_node = cur_node->next;
     }
+    return NULL;
+}
+
+//chechs if in that order there is nodes or it's empty
+MallocMetadata *find_free_node(int order, size_t data_size)// 3
+{
+    MeList *cur_list = free_blocks_arr[order];
+    if (cur_list->length == 0)
+        return NULL;
+
+    MallocMetadata *to_return = cur_list->dummy_head;
+    return to_return;
+}
+
+// this function should find for me what is the specific order
+// there might be case that in the very first order we get here has empty list in free_arr
+// so we should go to order+1 and check if there is nodes there
+MallocMetadata *find_specific_order(int order, size_t data_size)// 2
+{
+    if (order > 10)
+    {
+        return NULL;
+    }
+    // we have to split
+    MallocMetadata *element = find_free_node(order, data_size);
+    if (element == NULL)
+        find_specific_order(order + 1, data_size);
+
+    // found the specific order
     else
     {
-        // we have to split
-        MallocMetadata *new_buddy; // how to create new object?
-        new_buddy->size /= 2;
-        add_and_merge_buddies(new_buddy, order - 1);
-        element->size /= 2;
-        split_buddies(element, order - 1, size);
+        while (order >= 0)
+        {
+            size_t half_size = (element->size - sizeof(MallocMetadata)) / 2;
+
+            // reached to the maximum splits he can
+            if (data_size > half_size)
+            {
+                element->is_free = false;
+                return element;
+            }
+            else
+            {
+                MallocMetadata *buddy = split_free(element, order);
+                if (buddy == NULL)
+                    return NULL;
+                split_all(element, buddy);
+                order--;
+            }
+        }
     }
 }
-void split_free(int order)
-{
-}
 
-MallocMetadata *slpit_all()
-{
-}
 
-void remove_from_arr(MallocMetadata *element, int size)
+// assume that outer function calls this func 
+// it give it data_size and expects from it to return the most prefect node
+// definition most prefect : 
+// the smallest node with size that can fit data_size , and smallest in adress
+// and in the way of finiding that node we have to do splits
+MallocMetadata *find_prefect_node(size_t data_size)// 1
 {
     int order = 0;
     while (order < MAX_ORDER)
     {
-        if (pow(2, order) == element->size) // assumig we can use this, asked in PIAZZA already
+        if ((pow(2, order) * 128) >= (int)(data_size + sizeof(MallocMetadata)))
         {
             // we are done
             break;
@@ -304,7 +396,7 @@ void remove_from_arr(MallocMetadata *element, int size)
 
         order++;
     }
-    seperate_buddies(element, order, size);
+    return find_specific_order(order, data_size);
 }
 
 //*********************************************************************************** FUNCS ************************************************************
